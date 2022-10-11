499f0ea0f7e6539fece088b1fb7defb63c85fb20
ting-prado <ethel.prado1998@gmail.com>
2022-10-11 11:17:20 +0800

    navbar link highlighted if navigated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,42 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import '../styles/Navbar.css';
 import 'bootstrap/dist/js/bootstrap';
 import { useEffect } from 'react';
 
 const Navbar = () => {
+  let location = useLocation();
   useEffect(() => {
-    const links = document.querySelectorAll('.nav-link, .navbar-brand');
-    links.forEach((link) => {
-      link.addEventListener('click', (e) => {
-        const active = document.querySelector('.active');
-        active.classList.remove('active');
-        e.target.classList.contains('nav-link')
-          ? e.target.classList.add('active')
-          : document.querySelector('.home').classList.add('active');
-      });
-    });
-  });
+    document
+      .querySelectorAll('.nav-link')
+      .forEach((nav) => nav.classList.remove('active'));
+
+    switch (location.pathname) {
+      case '/':
+        document.querySelector('.home').classList.add('active');
+        break;
+      case '/agents':
+        document.querySelector('.agents').classList.add('active');
+        break;
+      case '/maps':
+        document.querySelector('.maps').classList.add('active');
+        break;
+      case '/weapons':
+        document.querySelector('.weapons').classList.add('active');
+        break;
+      default:
+        break;
+    }
+  }, [location]);
 
   return (
     <nav className="navbar navbar-expand-lg navbar-expand-md">
-      <Link to="/" className="navbar-brand">
+      <Link to="/" className="navbar-brand brand">
         <img
-          className="logo"
+          className="logo brand"
           src="https://images.hdqwalls.com/download/valorant-game-logo-4k-wi-2048x2048.jpg"
           alt="valorant logo"
         />
-        <p className="mb-0">Valorant</p>
+        <p className="mb-0 brand">Valorant</p>
       </Link>
       <button
         className="navbar-toggler"
@@ -37,22 +48,22 @@ const Navbar = () => {
       <div id="navCollapse" className="collapse navbar-collapse">
         <ul className="navbar-nav ms-auto d-flex gap-5">
           <li className="nav-item">
-            <Link to="/" className="home nav-link active">
+            <Link to="/" className="nav-link home">
               Home
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/agents" className="nav-link">
+            <Link to="/agents" className="nav-link agents">
               Agents
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/maps" className="nav-link">
+            <Link to="/maps" className="nav-link maps">
               Maps
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/weapons" className="nav-link">
+            <Link to="/weapons" className="nav-link weapons">
               Weapons
             </Link>
           </li>
